2a875795c7076d0cbb1b19ad6bfb4da554ad4c8d
hanieh <rahimihaniyeh58@gmail.com>
2026-09-04 20:01:33 +0330

    Add train test split

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Imports
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
-
+from sklearn.model_selection import train_test_split
 
 #Load Dataset
 housing = fetch_california_housing(as_frame=True)
@@ -18,3 +18,10 @@ print(df.isnull().sum())
 #Correlation
 print(df.corr()["MedHouseVal"].sort_values(ascending=False))
 
+#Define X and y
+X = df.drop("MedHouseVal",axis=1)
+y = df["MedHouseVal"]
+
+print(X,y)
+# Train / Test Split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
