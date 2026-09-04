e16a80e16c1771a181a0b14a6c9c194c3fbddbcc
hanieh <rahimihaniyeh58@gmail.com>
2026-09-04 20:09:20 +0330

    Add Linear regression model and evaluation metrics

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
 
 #Load Dataset
 housing = fetch_california_housing(as_frame=True)
@@ -25,3 +27,22 @@ y = df["MedHouseVal"]
 print(X,y)
 # Train / Test Split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+#Linear Regression
+model_linear = LinearRegression()
+
+model_linear.fit(X_train,y_train)
+
+y_pred_linear = model_linear.predict(X_test)
+
+# Evaluate Linear Regression
+mae_linear = mean_absolute_error(y_test,y_pred_linear)
+mse_linear = mean_squared_error(y_test,y_pred_linear)
+r2_linear = r2_score(y_test,y_pred_linear)
+
+print("\n Linear Regression: ")
+print("MAE: ",mae_linear)
+print("MSE: ",mse_linear)
+print("R2: ",r2_linear)
+
+
